d5d33751f775a96e2bc627e284fa843389bb963b
jsmith3763 <jsmith3763@gmail.com>
2023-01-17 07:22:10 -0800

    Finished

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+/* Can you find the needle in the haystack?
+
+Complete the method named findNeedle() that takes an array full of strings but containing only one "needle". A needle is just a String with the value of "needle".
+
+After your function finds the first needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle */
+class Main {
+    public static void main(String[] args) {
+        String[] haystack = {"the", "quick", "brown", "fox", "always", "pushes", "the", "needle", ".", "happy", "new", "year", "by.", "the", "way", ", Hello ! "};
+
+        String message = findNeedle(haystack);
+        System.out.println(message);
+    }
+
+    public static String findNeedle(String[] haystack) {
+        // Your code here
+        if(haystack == null) {
+            return "Haystack is null";
+        }
+
+        int hayStackLength = haystack.length;
+        int i = 0;
+
+        while(i < hayStackLength) {
+            if(haystack[i].equals("needle")) {
+                break;
+            }
+            i++;
+        }
+        return "Needle is in haystack at index: " + i;
+    }
+}
